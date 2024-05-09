d7a1a965731dfb085d6de58ac7438c6d44188667
BMWdejf <sablatura.david@gmail.com>
2024-05-09 22:30:27 +0200

    Create models for Contracts app

--- conctracts/models.py
+++ conctracts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Contract(models.Model):
+    contract_title = models.CharField(max_length=150, unique=True)
+    contract_description = models.TextField(blank=True)
+    active = models.BooleanField(default=False)
+    verbose_name_plural = 'Contracts'
+    def __str__(self):
+        return self.contract_title
+
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=150)
+    customer_surname = models.CharField(max_length=150)
+    customer_address = models.ForeignKey('CustomerAddress', on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+    verbose_name_plural = 'Customers'
+    def __str__(self):
+        return self.customer_title
+
+
+class CustomerAddress(models.Model):
+    customer_street = models.CharField(max_length=150)
+    customer_home_number = models.PositiveSmallIntegerField()
+    customer_city = models.CharField(max_length=150)
+    customer_country = models.CharField(max_length=150)
+    active = models.BooleanField(default=False)
+    verbose_name_plural = 'CustomerAddresses'
+    def __str__(self):
+        return self.customer_address
